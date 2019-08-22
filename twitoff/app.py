13b617e1b31ef2569e110f3065f4d7d7d012ed31
Matthew Harshman <mjharshman66@gmail.com>
2019-08-22 07:18:44 -0700

    set up routes with variables

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,16 +1,20 @@
 """Main application and routing logic for twitoff."""
 """ how to run: FLASK_APP=twitoff:APP flask run/shell"""
-from flask import Flask
-from .models import DB
+from flask import Flask, render_template, request
+from .models import DB, User
 
 
 def create_app():
     """Create and configure an instance of the Flask application"""
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = 'debug' # TODO change before deploying
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'hello twitoff'
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
+    
     return app
